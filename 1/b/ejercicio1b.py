b30e6bb4c147d03cf838a728c9c7174487964482
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-10-10 20:49:41 -0400

    ejercicio 1b

--- 1/b/ejercicio1b.py
+++ 1/b/ejercicio1b.py
@@ -1,11 +1,6 @@
-from ast import increment_lineno
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
-# %matplotlib inline
-# sns.set_style('darkgrid')
 
 datos = pd.read_csv('../../dataset/high_diamond_ranked_10min.csv')
 # print(datos)
