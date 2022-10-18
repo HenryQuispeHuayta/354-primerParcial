a7b0dd33cc2a73cd4d4811bfc46b0d201027dd53
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-10-18 08:24:31 -0400

    ejercicio 1c

--- 1/c/ejercicio1c.py
+++ 1/c/ejercicio1c.py
@@ -1,4 +1,19 @@
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 datos = pd.read_csv('../../dataset/high_diamond_ranked_10min.csv')
+
+sns.set_style('darkgrid')
+datos2 = datos.copy()
+cols = ['gameId', 'redFirstBlood', 'redKills', 'redEliteMonsters', 'redDragons','redTotalMinionsKilled',
+       'redTotalJungleMinionsKilled', 'redGoldDiff', 'redExperienceDiff', 'redCSPerMin', 'redGoldPerMin', 'redHeralds',
+       'blueGoldDiff', 'blueExperienceDiff', 'blueCSPerMin', 'blueGoldPerMin', 'blueTotalMinionsKilled']
+datos2 = datos2.drop(cols, axis = 1)
+
+datos2.hist(alpha = 0.7, figsize=(12,10), bins=5)
+
+# plot = datos.plot()
+
+plt.show()
