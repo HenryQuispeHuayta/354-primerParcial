37b7c98ae0aa5fc187428eabf9a839ce3e140a04
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-10-18 08:27:24 -0400

    ejercicio 1c

--- 1/c/ejercicio1c.py
+++ 1/c/ejercicio1c.py
@@ -7,10 +7,10 @@ datos = pd.read_csv('../../dataset/high_diamond_ranked_10min.csv')
 
 sns.set_style('darkgrid')
 datos2 = datos.copy()
-cols = ['gameId', 'redFirstBlood', 'redKills', 'redEliteMonsters', 'redDragons','redTotalMinionsKilled',
+columnas = ['gameId', 'redFirstBlood', 'redKills', 'redEliteMonsters', 'redDragons','redTotalMinionsKilled',
        'redTotalJungleMinionsKilled', 'redGoldDiff', 'redExperienceDiff', 'redCSPerMin', 'redGoldPerMin', 'redHeralds',
        'blueGoldDiff', 'blueExperienceDiff', 'blueCSPerMin', 'blueGoldPerMin', 'blueTotalMinionsKilled']
-datos2 = datos2.drop(cols, axis = 1)
+datos2 = datos2.drop(columnas, axis = 1)
 
 datos2.hist(alpha = 0.7, figsize=(12,10), bins=5)
 
