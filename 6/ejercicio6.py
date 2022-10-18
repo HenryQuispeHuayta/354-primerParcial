037300aa5d9210fad3aa26e5858a9978ef50ce61
Henry Quispe Huayta <henry.quispe.huayta@gmail.com>
2022-10-18 09:20:26 -0400

    ejercicio 6

--- 6/ejercicio6.py
+++ 6/ejercicio6.py
@@ -1,7 +1,27 @@
+# from ._base import load_breast_cancer
+# from importlib import resources
+# from typing.io import BinaryIO, TextIO
+
+from ast import mod
 import numpy as np
-from sklearn import tree
-from sklearn import datasets
+import pandas as pd
+# from sklearn import tree
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.tree import plot_tree
+# from sklearn import datasets
 
-iris = datasets.load_iris()
+datos = pd.read_csv('../dataset/high_diamond_ranked_10min.csv')
 
-print(iris)
+pd.get_dummies(data=datos, drop_first=True)
+
+a = datos.drop(columns='blueWins')
+b = datos.blueWins
+
+# fit()
+modelo = DecisionTreeClassifier()
+
+modelo.fit(X=a, y=b)
+DecisionTreeClassifier()
+
+plot_tree(decision_tree=modelo, feature_names=a.columns, filled=True)
+# print(datos)
